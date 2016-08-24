e9ebce62d93ec671128a1b16b3b796f115f18e9a
Evgeniy Krupen <evgeniy_krupen@epam.com>
2016-08-24 12:20:45 +0300

    all task with pep8

--- task1/task3.py
+++ task1/task3.py
@@ -3,67 +3,75 @@ import datetime
 import json
 import psutil
 import schedule
+
 Config = configparser.ConfigParser()
 Config.read("config3.ini")
 file_type = Config.get('common', 'output')
 interval = Config.get('common', 'interval')
 
 counter = 1
+
+
 # function logging to TXT
-def ToTxtFile():
-    '''write to txt file'''
-    #define variables
+def totxtfile():
+    """write to txt file"""
+    # define variables
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
-    VMEMORY = psutil.virtual_memory()
-    MEMORY = psutil.swap_memory()
-    CPU_LOAD = psutil.cpu_percent(interval=1, percpu=True)
-    DISK_IO = psutil.disk_usage('/')
-    NETWORK = psutil.net_io_counters(pernic=False)
+    vmemory = psutil.virtual_memory()
+    memory = psutil.swap_memory()
+    cpu_load = psutil.cpu_percent(interval=1, percpu=True)
+    disk_io = psutil.disk_usage('/')
+    network = psutil.net_io_counters(pernic=False)
     global counter
-    #write to file
+    # write to file
     file = open("output.txt", "a")
-    file.write("\n###Snapshot {0}: TimeStamp {1}\n".format(counter,timestamp))
-    file.write("CPU load (%): {0}\n".format(CPU_LOAD))
-    file.write("Memory usage (Mb): used {0}, free {1}\n".format((VMEMORY[4]/(1024**2)).__round__(2),(VMEMORY[5]/(1024**2)).__round__(2)))
-    file.write("Swap Memory stat (Mb): total {0}, used {1}\n".format((VMEMORY[0]/(1024**2)).__round__(2),(VMEMORY[1]/(1024**2)).__round__(2)))
-    file.write("Disk usage (Mb): total {0}, free {1}\n".format((DISK_IO[0]/(1024**2)).__round__(2),(DISK_IO[2]/(1024**2)).__round__(2)))
-    file.write("Network stats: \n{0}\n".format(NETWORK))
+    file.write("\n###Snapshot {0}: TimeStamp {1}\n".format(counter, timestamp))
+    file.write("CPU load (%): {0}\n".format(cpu_load))
+    file.write("Memory usage (Mb): used {0}, free {1}\n".format((vmemory[4] / (1024 ** 2)).__round__(2),
+                                                                (vmemory[5] / (1024 ** 2)).__round__(2)))
+    file.write("Swap Memory stat (Mb): total {0}, used {1}\n".format((memory[0] / (1024 ** 2)).__round__(2),
+                                                                     (memory[1] / (1024 ** 2)).__round__(2)))
+    file.write("Disk usage (Mb): total {0}, free {1}\n".format((disk_io[0] / (1024 ** 2)).__round__(2),
+                                                               (disk_io[2] / (1024 ** 2)).__round__(2)))
+    file.write("Network stats: \n{0}\n".format(network))
     file.write("\n============================================\n")
     counter += 1
     file.close()
 
+
 # function logging to Json
-def ToJsonFile():
+def tojsonfile():
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
-    VMEMORY = psutil.virtual_memory()
-    MEMORY = psutil.swap_memory()
-    CPU_LOAD = psutil.cpu_percent(interval=1, percpu=True)
-    DISK_IO = psutil.disk_usage('/')
-    NETWORK = psutil.net_io_counters(pernic=False)
+    vmemory = psutil.virtual_memory()
+    memory = psutil.swap_memory()
+    cpu_load = psutil.cpu_percent(interval=1, percpu=True)
+    disk_io = psutil.disk_usage('/')
+    network = psutil.net_io_counters(pernic=False)
     json_file = open("outputdata.json", "a")
     global counter
     snapshot = {
         'Timestamp': timestamp,
-        'CPU_load' : CPU_LOAD,
-        'Vmemory' : VMEMORY,
-        'Memory' : MEMORY,
-        'IO_info': DISK_IO,
-        'Network_info': NETWORK
+        'CPU_load': cpu_load,
+        'Vmemory': vmemory,
+        'Memory': memory,
+        'IO_info': disk_io,
+        'Network_info': network
     }
     data = ['SNAPSHOT' + str(counter) + ": " + str(timestamp) + ": ", snapshot]
     json.dump(data, json_file, indent=3, sort_keys=True)
     counter += 1
     json_file.close()
 
+
 # condition (type file)
 if file_type == "txt":
     print('Output file type = ' + file_type + ', interval = ' + interval + ' minutes')
-    schedule.every(int(interval)).minutes.do(ToTxtFile)
+    schedule.every(int(interval)).minutes.do(totxtfile)
 elif file_type == "json":
     print('Output filetype = ' + file_type + ', interval = ' + interval + ' minutes')
-    schedule.every(int(interval)).seconds.do(ToJsonFile)
+    schedule.every(int(interval)).seconds.do(tojsonfile)
 else:
     print("Bad filetype in config file")
     quit()
 while True:
-	schedule.run_pending()
+    schedule.run_pending()
--- task1/task4.py
+++ task1/task4.py
@@ -3,7 +3,8 @@ import datetime
 import json
 import psutil
 import schedule
-import time
+
+# import time
 
 # parser config file
 Config = configparser.ConfigParser()
@@ -11,26 +12,30 @@ Config.read("config3.ini")
 file_type = Config.get('common', 'output')
 interval = Config.get('common', 'interval')
 
-#class with my variables
-class myvar:
-    counter = 1
-    'this is my parent class'
-    def __init__(self):
-         self.timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
-         self.counter = 1
-         self.VMEMORY = psutil.virtual_memory()
-         self.MEMORY = psutil.swap_memory()
-         self.CPU_LOAD = psutil.cpu_percent(interval=1, percpu=True)
-         self.DISK_IO = psutil.disk_usage('/')
-         self.NETWORK = psutil.net_io_counters(pernic=False)
 
-#class for txt to file (with Inheritance)
-class TXTClass(myvar):
-    'class for TXT-TO-FILE'
-    def ToTxtFile(self):
-        '''write to txt file'''
+# class with my variables
+class MyVar:
+    counter = 1
+    "this is my parent class"
+
+    def __init__(self):
+        self.timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+        self.counter = 1
+        self.VMEMORY = psutil.virtual_memory()
+        self.MEMORY = psutil.swap_memory()
+        self.CPU_LOAD = psutil.cpu_percent(interval=1, percpu=True)
+        self.DISK_IO = psutil.disk_usage('/')
+        self.NETWORK = psutil.net_io_counters(pernic=False)
+
+
+# class for txt to file (with Inheritance)
+class TXTClass(MyVar):
+    """class for TXT-TO-FILE"""
+
+    def totxtfile(self):
+        """write to txt file"""
         file = open("output.txt", "a")
-        file.write("\n###Snapshot {0}: TimeStamp {1}\n".format(myvar.counter, self.timestamp))
+        file.write("\n###Snapshot {0}: TimeStamp {1}\n".format(MyVar.counter, self.timestamp))
         file.write("CPU load (%): {0}\n".format(self.CPU_LOAD))
         file.write("Memory usage (Mb): used {0}, free {1}\n".format((self.VMEMORY[4] / (1024 ** 2)).__round__(2),
                                                                     (self.VMEMORY[5] / (1024 ** 2)).__round__(2)))
@@ -41,38 +46,38 @@ class TXTClass(myvar):
         file.write("Network stats: \n{0}\n".format(self.NETWORK))
         file.write("\n============================================\n")
         file.close()
-        myvar.counter += 1
+        MyVar.counter += 1
 
-#class for txt to json (with Inheritance)
-class JSONclass(myvar):
-    def ToJsonFile(self):
-       json_file = open("outputdata.json", "a")
-       snapshot = {
-           'Timestamp': self.timestamp,
-           'CPU_load': self.CPU_LOAD,
-           'Vmemory': self.VMEMORY,
-           'Memory': self.MEMORY,
-           'IO_info': self.DISK_IO,
-           'Network_info': self.NETWORK
-       }
-       data = ['SNAPSHOT ' + str(myvar.counter) + ": " + str(self.timestamp) + ": ", snapshot]
-       json.dump(data, json_file, indent=3, sort_keys=True)
-       json_file.close()
-       myvar.counter += 1
+
+# class for txt to json (with Inheritance)
+class JSONclass(MyVar):
+    def tojsonfile(self):
+        json_file = open("outputdata.json", "a")
+        snapshot = {
+            'Timestamp': self.timestamp,
+            'CPU_load': self.CPU_LOAD,
+            'Vmemory': self.VMEMORY,
+            'Memory': self.MEMORY,
+            'IO_info': self.DISK_IO,
+            'Network_info': self.NETWORK
+        }
+        data = ['SNAPSHOT ' + str(MyVar.counter) + ": " + str(self.timestamp) + ": ", snapshot]
+        json.dump(data, json_file, indent=3, sort_keys=True)
+        json_file.close()
+        MyVar.counter += 1
+
 
 # initialization with condition
 def run():
     if file_type == "txt":
-       txt_obj = TXTClass()
-       txt_obj.ToTxtFile()
+        txt_obj = TXTClass()
+        txt_obj.totxtfile()
 
     elif file_type == "json":
-       txt_obj = JSONclass()
-       txt_obj.ToJsonFile()
+        txt_obj = JSONclass()
+        txt_obj.tojsonfile()
 
-schedule.every(int(5)).seconds.do(run)
+
+schedule.every(int(interval)).seconds.do(run)
 while True:
     schedule.run_pending()
-
-
-
